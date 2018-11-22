931ce6afd2592f4f83d44ff3a4edf7ff8e8a4036
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-11-22 01:02:09 -0800

    Revert "temporary crazy test with 4000 circles and 400 relevants"
    This reverts commit bdd5e96f9cf24af1339481b12a1d8f4875642d81.

--- example_client.lua
+++ example_client.lua
@@ -3,7 +3,7 @@ local client = cs.client
 
 
 client.enabled = true
-client.start('207.254.45.246:22122') -- IP address ('127.0.0.1' is same computer) and port of server
+client.start('127.0.0.1:22122') -- IP address ('127.0.0.1' is same computer) and port of server
 
 
 -- Client connects to server. It gets a unique `id` to identify it.
@@ -42,21 +42,18 @@ end
 
 function client.draw()
     if client.connected then
+        -- Draw our own mouse in a special way (bigger radius)
+        love.graphics.circle('fill', home.mouse.x, home.mouse.y, 40, 40)
+
         -- Draw other mice
         for id, mouse in pairs(share.mice) do
             if id ~= client.id then -- Only draw others' mice this way
-                love.graphics.setColor(mouse.r, mouse.g, mouse.b)
                 love.graphics.circle('fill', mouse.x, mouse.y, 30, 30)
             end
         end
 
-        -- Draw our own mouse in a special way (bigger radius)
-        love.graphics.setColor(1, 1, 1)
-        love.graphics.circle('fill', home.mouse.x, home.mouse.y, 40, 40)
-
         -- Draw our ping
         love.graphics.print('ping: ' .. client.getPing(), 20, 20)
-        love.graphics.print('\nfps: ' .. love.timer.getFPS(), 20, 20)
     else
         love.graphics.print('not connected', 20, 20)
     end
